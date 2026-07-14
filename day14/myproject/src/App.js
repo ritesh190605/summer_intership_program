a0ae70bfb681e6ea0da57fcdb58904a3942b568c
Ritesh Yadav <riteshblackdotsco@gmail.com>
2026-07-15 02:40:51 +0530

    commit

---
+++ day14/myproject/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import About from './components/About';
+import Contact from './components/Contact'
+import Home from './components/Home'
+import MyProject from './components/MyProject'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Home/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
